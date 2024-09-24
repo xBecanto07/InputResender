c570e51c056f17f631de846467e873ac195a7e8f
xBecanto07 <ja.malinak@gmail.com>
2024-09-24 09:53:31 +0200

    Added ArgParser testing, fixed some parsing isssues and added support for switches.
    Warning: I still believe the testing can be greatly expanded.

--- Components.Library/ComponentSystem/ArgParser.cs
+++ Components.Library/ComponentSystem/ArgParser.cs
@@ -10,13 +10,23 @@ public class ArgParser {
 		public int Position;
 		public string Name;
 		public string Value;
+		/// <summary>Marks that the value was explicitly specified (e.g. "A=1":true vs "A 1":false).</summary>
+		public bool ValSpecified;
 
 		override public string ToString () => $"{Name}#{Position} = {Value}";
+
+		public Arg ( int pos, string name, string value, bool valSpec ) {
+			Position = pos;
+			Name = name;
+			Value = value;
+			ValSpecified = valSpec;
+		}
 
 		public Arg ( string line, ref int pos, int ID ) {
 			Position = ID;
 			Name = string.Empty;
 			Value = string.Empty;
+			ValSpecified = false;
 
 			bool readingName = true;
 			bool isEscaped = false;
@@ -25,13 +35,13 @@ public class ArgParser {
 
 			for ( int N = line.Length; pos < N; pos++ ) {
 				if ( isEscaped ) {
-					switch ( line[pos] ) {
-						case 'n': arg += '\n'; break;
-						case 'r': arg += '\r'; break;
-						case 't': arg += '\t'; break;
-						case '0': arg += '\0'; break;
-						default: arg += line[pos]; break;
-					}
+					arg += line[pos] switch {
+						'n' => '\n',
+						'r' => '\r',
+						't' => '\t',
+						'0' => '\0',
+						_ => (object)line[pos],
+					};
 					isEscaped = false;
 					continue;
 				}
@@ -55,6 +65,7 @@ public class ArgParser {
 					if ( readingName ) {
 						Name = arg[..^1];
 						readingName = false;
+						ValSpecified = true;
 						arg = string.Empty;
 					} else {
 						throw new ArgumentException ( $"Unexpected '=' at {pos}" );
@@ -64,14 +75,20 @@ public class ArgParser {
 		}
 	}
 
+	private readonly Dictionary<string, Arg> SwitchesName;
+	private readonly Dictionary<char, Arg> SwitchesChar;
+	/// <summary>Positional args only. Position-independent switches should be extracted via <see cref="ArgParser.RegisterSwitch(char, string, string)"/> before usage (they'll be stored in SwitchesName and SwitchesChar).</summary>
 	private readonly List<Arg> Args;
 	private readonly Action<string> Output;
 	public enum ErrLvl { None, Minimal, Normal, Full }
 	public ErrLvl ErrorLevel;
 
+	public const int ErrArgDup = 1;
 	public ArgParser ( string args, Action<string> output, ErrLvl errLvl = ErrLvl.Normal ) {
 		ErrorLevel = errLvl;
 		Args = new ();
+		SwitchesChar = new ();
+		SwitchesName = new ();
 		Output = output;
 
 		if ( string.IsNullOrEmpty ( args ) ) return;
@@ -89,13 +106,14 @@ public class ArgParser {
 				throw new ArgumentException ( $"Unexpected character '{args[i]}' at {i}." );
 			}
 
-			if ( nextArg.Name.StartsWith ( '-' ) ) {
+			/*if ( nextArg.Name.StartsWith ( '-' ) ) {
 				foreach ( var arg in Args ) {
 					if ( arg.Name != nextArg.Name ) continue;
-					Error ( $"Argument '{nextArg.Name}' is duplicated.", 1, true, 0 );
-					return;
+					arg.Value = nextArg.Value;
+					//Error ( $"Argument '{nextArg.Name}' is duplicated.", ErrArgDup, true, 0 );
+					//return;
 				}
-			}
+			}*/
 
 			Args.Add ( nextArg );
 		}
@@ -123,43 +141,72 @@ public class ArgParser {
 	public int ArgC => Args.Count;
 	private Arg this[int id] => (id >= ArgC | id < -ArgC) ? null : Args[id];
 	private Arg this[string key] {
-		get => Args.FirstOrDefault ( arg => arg.Name == key );
+		get {
+			if ( key.StartsWith ( "--" ) ) return SwitchesName.ContainsKey ( key ) ? SwitchesName[key] : null;
+			if ( key.StartsWith ( "-" ) && key.Length == 2 ) return SwitchesChar.ContainsKey ( key[1] ) ? SwitchesChar[key[1]] : null;
+			return Args.FirstOrDefault ( arg => arg.Name == key );
+		}
 	}
 
+	public const int ErrArgNotFoundByName = 2;
+	public const int ErrArgNoValue = 3;
+	public const int ErrEmptyKey = 12;
+	public const int ErrSwitchNameNotFound = 13;
+	public const int ErrSwitchCharNotFound = 14;
 	private string Get ( string key, string dsc, bool shouldThrow = false ) {
-		var arg = this[key];
-		if ( arg == null ) return dsc == null ? string.Empty : Error ( $"Argument '{key}' not found. {dsc}", 2, shouldThrow, string.Empty );
+		Arg arg = null;
+		if ( string.IsNullOrEmpty ( key ) ) return Error ( "Key cannot be empty", ErrEmptyKey, shouldThrow, string.Empty );
+		if ( key.StartsWith ( "--" ) ) {
+			if ( !SwitchesName.ContainsKey ( key ) )
+				return Error ( $"Switch {key} not found.", ErrSwitchNameNotFound, shouldThrow, string.Empty );
+			arg = SwitchesName[key];
+		} else if ( key.StartsWith ( "-" ) && key.Length == 2 ) {
+			if ( !SwitchesChar.ContainsKey ( key[1] ))
+				return Error ($"Switch {key} not found.", ErrSwitchCharNotFound, shouldThrow, string.Empty );
+			arg = SwitchesChar[key[1]];
+		} else arg = this[key];
 
-		if ( !GetSepValue ( arg ) ) return dsc == null ? string.Empty : Error ( $"Argument '{key}' has no value. {dsc}", 3, shouldThrow, string.Empty );
+		if ( arg == null ) return dsc == null ? string.Empty : Error ( $"Argument '{key}' not found. {dsc}", ErrArgNotFoundByName, shouldThrow, string.Empty );
+
+		if ( !GetSepValue ( arg ) ) return dsc == null ? string.Empty : Error ( $"Argument '{key}' has no value. {dsc}", ErrArgNoValue, shouldThrow, string.Empty );
 		return arg.Value;
 	}
+	public const int ErrArgNotFoundByID = 4;
 	private string Get ( int id, string dsc = null, bool shouldThrow = false ) {
 		var arg = this[id];
-		if ( arg == null ) return dsc == null ? string.Empty : Error ( $"Argument #{id} not found. {dsc}", 4, shouldThrow, string.Empty );
-		return arg.Name;
+		if ( arg == null ) return dsc == null ? string.Empty : Error ( $"Argument #{id} not found. {dsc}", ErrArgNotFoundByID, shouldThrow, string.Empty );
+		return string.IsNullOrEmpty ( arg.Value ) ? arg.Name : arg.Value;
 	}
 
 	private bool GetSepValue ( Arg arg ) {
 		if ( !string.IsNullOrEmpty ( arg.Value ) ) return true;
 		int next = arg.Position + 1;
-		if ( next >= ArgC || !string.IsNullOrEmpty ( Args[next].Value ) || Args[next].Name.StartsWith ( '-' ) ) {
+		if ( next >= ArgC || !string.IsNullOrEmpty ( Args[next].Value ) ) {
 			return false;
 		}
 
-		arg.Value = Args[next].Name;
+		string s = Args[next].Name;
+		if ( string.IsNullOrEmpty ( s ) ) return false;
+		if ( s.StartsWith ( "--" ) ) return false;
+		if ( s.Length == 2 && s[0] == '-' && char.IsAsciiLetter ( s[1] ) ) return false;
+
+		arg.Value = s;
 		Args.RemoveAt ( next );
 		for ( int i = next; i < ArgC; i++ ) Args[i].Position--;
 		return true;
 	}
 
+	public const int ErrArgParseNotFound = 5;
+	public const int ErrArgParseBadType = 6;
+	public const int ErrArgParseProblem = 7;
 	private T? Parse<T> ( string arg, object id, string dsc, Func<string, T> parser, bool shouldThrow = false ) where T : struct {
-		if ( arg == null ) return Error<T?> ( $"Argument '{id}' not found. {dsc}", 5, shouldThrow, null );
+		if ( arg == null ) return Error<T?> ( $"Argument '{id}' not found. {dsc}", ErrArgParseNotFound, shouldThrow, null );
 		try {
 			return parser ( arg );
 		} catch ( FormatException ) {
-			return Error<T?> ( $"Argument '{id}' is not {typeof ( T ).Name}. {dsc}", 6, shouldThrow, null );
+			return Error<T?> ( $"Argument '{id}' is not {typeof ( T ).Name}. {dsc}", ErrArgParseBadType, shouldThrow, null );
 		} catch ( Exception ex ) {
-			return Error<T?> ( $"Problem with parsing argument '{id}' ({arg}). {dsc}\n\t{ex.Message}", 7, shouldThrow, null );
+			return Error<T?> ( $"Problem with parsing argument '{id}' ({arg}). {dsc}\n\t{ex.Message}", ErrArgParseProblem, shouldThrow, null );
 		}
 	}
 
@@ -167,20 +214,23 @@ public class ArgParser {
 	public double? Double ( string key, string dsc, bool shouldThrow = false ) => Parse ( Get ( key, dsc ), key, dsc, double.Parse );
 	public int? Int ( int id, string dsc, bool shouldThrow = false ) => Parse ( Get ( id, dsc ), id, dsc, int.Parse );
 	public int? Int ( string key, string dsc, bool shouldThrow = false ) => Parse ( Get ( key, dsc ), key, dsc, int.Parse );
+
+	public const int ErrStringTooShortByID = 8;
 	public string String ( int id, string dsc, int min = 0, bool shouldThrow = false ) {
 		string ret = Get ( id, dsc );
 		if ( ret != null && ret.Length < min )
-			return Error ( $"Argument #{id}({ret}) is too short. {dsc}", 8, shouldThrow, string.Empty );
+			return Error ( $"Argument #{id}({ret}) is too short. {dsc}", ErrStringTooShortByID, shouldThrow, string.Empty );
 		return ret;
 	}
+	public const int ErrStringTooShortByName = 9;
 	public string String ( string key, string dsc, int min = 0, bool shouldThrow = false ) {
 		string ret = Get ( key, dsc );
 		if ( ret != null && ret.Length < min )
-			return Error ( $"Argument '{key}'({ret}) is too short. {dsc}", 9, shouldThrow, string.Empty );
+			return Error ( $"Argument '{key}'({ret}) is too short. {dsc}", ErrStringTooShortByName, shouldThrow, string.Empty );
 		return ret;
 	}
-	public bool Present ( int id ) => Args.Any ( arg => arg.Position == id );
-	public bool Present ( string key ) => Args.Any ( arg => arg.Name == key );
+	public bool Present ( int id ) => this[id] != null;
+	public bool Present ( string key ) => this[key] != null;
 
 	public bool HasValue ( int id, bool tryLoadValue ) => HasValue ( this[id], tryLoadValue );
 	public bool HasValue ( string key, bool tryLoadValue ) => HasValue ( this[key], tryLoadValue );
@@ -197,17 +247,83 @@ public class ArgParser {
 			SB.AppendLine ( $"Arg #{arg.Position}: '{arg.Name}' = '{arg.Value}'" );
 		return SB.ToString ();
 	}
+
+	public void RegisterSwitch ( char sw, string name, string defVal = null ) {
+		if ( string.IsNullOrEmpty ( name ) ) throw new ArgumentNullException ( nameof ( name ) );
+		if ( !char.IsAsciiLetter ( sw ) ) throw new ArgumentException ( $"Switch '{sw}' is not a valid character.", nameof ( sw ) );
+
+		string swStr = $"-{sw}";
+		name = $"--{name}";
+		Arg arg = null;
+		for ( int i = ArgC - 1; i >= 0; i-- ) {
+			if ( Args[i].Name != swStr && Args[i].Name != name ) continue;
+			Arg extracted = ExtractSwitch ( i, defVal );
+			if ( extracted == null ) continue;
+
+			if ( arg == null ) arg = extracted;
+			else if ( extracted.Value == null ) continue;
+			else if ( extracted.ValSpecified ) {
+				if ( string.IsNullOrEmpty ( arg.Value ) ) {
+					arg.Value = string.IsNullOrEmpty ( extracted.Value ) ? defVal : extracted.Value;
+					arg.ValSpecified = true;
+				} else continue;
+			} else if ( arg.Value == null ) arg.Value = extracted.Value;
+			else if ( arg.Value != defVal ) continue;
+			else arg.Value = extracted.Value;
+		}
+
+		if (arg != null) {
+			if ( arg.Value == null ) arg.Value = defVal ?? string.Empty;
+			SwitchesChar[sw] = arg;
+			SwitchesName[name] = arg;
+		} else if (defVal != null) {
+			arg = new ( -1, name, defVal, true );
+			SwitchesChar[sw] = arg;
+			SwitchesName[name] = arg;
+		}
+	}
+	private Arg ExtractSwitch ( int id, string defVal = null ) {
+		Arg ret = Args[id];
+
+		if ( defVal != null ) {
+			if (string.IsNullOrEmpty(ret.Value)) {
+				if ( ret.ValSpecified ) ret.Value = defVal;
+				else {
+					if ( !GetSepValue ( ret ) ) ret.Value = null;
+					else if (string.IsNullOrEmpty(ret.Value)) ret.Value = defVal;
+					// ret.Value has changed due to GetSepValue()
+				}
+			}
+
+			/*if ( ret.ValSpecified ) {
+				if ( string.IsNullOrEmpty ( ret.Value ) ) ret.Value = defVal;
+			} else if ( !ret.ValSpecified && string.IsNullOrEmpty ( ret.Value ) ) {
+				// Value not assigned, try to get value from next argument or assign default
+
+				if ( !GetSepValue ( ret ) ) ret.Value = null;
+				else if ( ret.Value == defVal ) ret.Value = null;
+				else if ( string.IsNullOrEmpty ( ret.Value ) ) ret.Value = defVal;
+			}*/
+		}
+
+		Args.RemoveAt ( id );
+		for ( int i = ArgC - 1; i >= id; i-- ) Args[i].Position--;
+		return ret;
+	}
+
+	public const int ErrEnumInvalid = 10;
+	public const int ErrEnumUndefined = 11;
 	public T EnumC<T> ( int id, string dsc, bool shouldThrow = false ) where T : struct {
 		string arg = Get ( id, dsc );
 		if ( !Enum.TryParse ( arg, true, out T ret ) )
-			return Error ( $"Argument #{id}({arg}) is not a valid {typeof ( T ).Name}. {dsc}", 10, shouldThrow, default ( T ) );
+			return Error ( $"Argument #{id}({arg}) is not a valid {typeof ( T ).Name}. {dsc}", ErrEnumInvalid, shouldThrow, default ( T ) );
 		if ( !Enum.IsDefined ( typeof ( T ), ret ) )
-			return Error ( $"Argument #{id}({arg}) was not found in {typeof ( T ).Name}. {dsc}", 11, shouldThrow, default ( T ) );
+			return Error ( $"Argument #{id}({arg}) was not found in {typeof ( T ).Name}. {dsc}", ErrEnumUndefined, shouldThrow, default ( T ) );
 		return ret;
 	}
 
 	private T Error<T> ( string message, int errID, bool shouldThrow, T defVal ) {
-		if ( shouldThrow ) throw new ArgumentException ( message );
+		if ( shouldThrow | (Output == null) ) throw new ArgumentException ( $"{message} (ArgParseErr#{errID})" );
 		if ( ErrorLevel != ErrLvl.None ) {
 			if ( ErrorLevel == ErrLvl.Minimal ) Output ( $"ArgParseErr#{errID}" );
 			else if ( ErrorLevel == ErrLvl.Normal ) Output ( message );
---
+++ Components.LibraryTests/ArgParserTest.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Components.Library;
+using FluentAssertions;
+using Xunit;
+
+namespace Components.LibraryTests; 
+public class ArgParserTest {
+	[Fact]
+	public void BasicArgs () {
+		var parser = new ArgParser ( "A1 A2 A3", null );
+		parser.String ( 0, "Argument 1" ).Should ().Be ( "A1" );
+		parser.String ( 1, "Argument 2" ).Should ().Be ( "A2" );
+		parser.String ( 2, "Argument 3" ).Should ().Be ( "A3" );
+		Action extraArg = () => parser.String ( 3, "Extra argument" );
+		AssertError ( extraArg, "Argument #3 not found", ArgParser.ErrArgNotFoundByID );
+
+		parser.String ( 0, null ).Should ().Be ( "A1" );
+		parser.String ( 1, null ).Should ().Be ( "A2" );
+		parser.String ( 2, null ).Should ().Be ( "A3" );
+		parser.String ( 3, null ).Should ().NotBeNull ().And.BeEmpty ();
+	}
+
+	[Fact]
+	public void NamedArgs () {
+		var parser = new ArgParser ( "X=1 L=2 A1=3", null );
+		parser.Int (0, "X").Should ().Be ( 1 );
+		parser.Int (1, "L").Should ().Be ( 2 );
+		parser.Int (2, "A1").Should ().Be ( 3 );
+		Action extraArg = () => parser.Int (3, "A2");
+		AssertError ( extraArg, "Argument #3 not found", ArgParser.ErrArgNotFoundByID );
+
+		parser.Int ( "X", "X" ).Should ().Be ( 1 );
+		parser.Int ( "L", "L" ).Should ().Be ( 2 );
+		parser.Int ( "A1", "A1" ).Should ().Be ( 3 );
+		extraArg = () => parser.Int ( "A2", "A2" );
+		AssertError ( extraArg, "Argument 'A2' not found", ArgParser.ErrArgNotFoundByName );
+	}
+
+	[Fact]
+	public void NamedArgsReversedOrderByID () {
+		var parser = new ArgParser ( "X=1 L=2 A1=3", null );
+		Action extraArg = () => parser.Int ( 3, "A2" );
+		AssertError ( extraArg, "Argument #3 not found", ArgParser.ErrArgNotFoundByID );
+		parser.Int ( 2, "A1" ).Should ().Be ( 3 );
+		parser.Int ( 1, "L" ).Should ().Be ( 2 );
+		parser.Int ( 0, "X" ).Should ().Be ( 1 );
+	}
+
+	[Fact]
+	public void NamedArgsReversedOrderByName () {
+		var parser = new ArgParser ( "X=1 L=2 A1=3", null );
+		Action extraArg = () => parser.Int ( "A2", "A2" );
+		AssertError ( extraArg, "Argument 'A2' not found", ArgParser.ErrArgNotFoundByName );
+		parser.Int ( "A1", "A1" ).Should ().Be ( 3 );
+		parser.Int ( "L", "L" ).Should ().Be ( 2 );
+		parser.Int ( "X", "X" ).Should ().Be ( 1 );
+	}
+
+	[Fact]
+	public void SwitchesRecognizedAndLoaded () {
+		var parser = new ArgParser ( "-a --all -a --something -b -f=5", null );
+		parser.RegisterSwitch ( 'a', "all", "0" );
+		parser.RegisterSwitch ( 'b', "bsdf", "0" );
+		parser.RegisterSwitch ( 'f', "fdsa", "0" );
+		parser.RegisterSwitch ( 's', "something", "0" );
+
+		parser.Present ( "-a" ).Should ().BeTrue ();
+		parser.Present ( "--all" ).Should ().BeTrue ();
+		parser.Present ( "--something" ).Should ().BeTrue ();
+		parser.Present ( "-b" ).Should ().BeTrue ();
+		parser.Present ( "-f" ).Should ().BeTrue ();
+
+		parser.HasValue ( "-a", true ).Should ().BeTrue ();
+		parser.HasValue ( "--all", true ).Should ().BeTrue ();
+		parser.HasValue ( "--something", true ).Should ().BeTrue ();
+		parser.HasValue ( "-b", true ).Should ().BeTrue ();
+		parser.HasValue ( "-f", true ).Should ().BeTrue ();
+
+		parser.Int ( "-a", "all" ).Should ().Be ( 0 );
+		parser.Int ( "--all", "all" ).Should ().Be ( 0 );
+		parser.Int ( "--something", "something" ).Should ().Be ( 0 );
+		parser.Int ( "-b", "bsdf" ).Should ().Be ( 0 );
+		parser.Int ( "-f", "f" ).Should ().Be ( 5 );
+	}
+
+	[Fact]
+	public void UndefinedSwitchHasDefaultValue () {
+		var parser = new ArgParser ( "-a 3", null );
+		parser.RegisterSwitch ( 'b', "beta", "5" );
+		parser.Present ( "-b" ).Should ().BeTrue ();
+		parser.HasValue ( "-b", true ).Should ().BeTrue ();
+		parser.Int ( "-b", "beta" ).Should ().Be ( 5 );
+	}
+
+	[Fact]
+	public void OptionalSwitchStayesUndefined () {
+		var parser = new ArgParser ( "-a 5", null );
+		parser.RegisterSwitch ( 'b', "beta", null );
+		parser.Present ( "-b" ).Should ().BeFalse ();
+		AssertError ( () => parser.Int ( "-b", "beta" ), "Switch -b not found.", ArgParser.ErrSwitchCharNotFound );
+	}
+
+	[Fact]
+	public void SwitchesMultipleValuesExplicit () {
+		var parser = new ArgParser ( "-a -a=1 -a=2 -a=3", null );
+		parser.RegisterSwitch ( 'a', "asdf", "0" );
+		parser.Present ( "-a" ).Should ().BeTrue ();
+		parser.HasValue ( "-a", true ).Should ().BeTrue ();
+		parser.Int ( "-a", "asdf" ).Should ().Be ( 3 );
+	}
+
+	[Fact]
+	public void SwitchWithSingleValueExplicit () {
+		var parser = new ArgParser ("-a -a -a=4 -a -a", null);
+		parser.RegisterSwitch ( 'a', "asdf", "0" );
+		parser.Present ( "-a" ).Should ().BeTrue ();
+		parser.HasValue ( "-a", true ).Should ().BeTrue ();
+		parser.Int ( "-a", "asdf" ).Should ().Be ( 4 );
+	}
+
+	[Fact]
+	public void SwitchesMultipleValuesReversedOrderExplicit () {
+		var parser = new ArgParser ( "-a=3 -a=2 -a=1 -a", null );
+		parser.RegisterSwitch ( 'a', "a", "0" );
+		parser.Present ( "-a" ).Should ().BeTrue ();
+		parser.HasValue ( "-a", true ).Should ().BeTrue ();
+		parser.Int ( "-a", "asdf" ).Should ().Be ( 1 );
+	}
+
+	[Fact]
+	public void SwitchesMultipleValuesImplicit () {
+		var parser = new ArgParser ( "-a -a 1 -a 2 -a 3", null );
+		parser.RegisterSwitch ( 'a', "asdf", "0" );
+		parser.Present ( "-a" ).Should ().BeTrue ();
+		parser.HasValue ( "-a", true ).Should ().BeTrue ();
+		parser.Int ( "-a", "asdf" ).Should ().Be ( 3 );
+	}
+
+	[Fact]
+	public void SwitchWithSingleValueImplicit () {
+		var parser = new ArgParser ( "-a -a -a 4 -a -a", null );
+		parser.RegisterSwitch ( 'a', "asdf", "0" );
+		parser.Present ( "-a" ).Should ().BeTrue ();
+		parser.HasValue ( "-a", true ).Should ().BeTrue ();
+		parser.Int ( "-a", "asdf" ).Should ().Be ( 4 );
+	}
+
+	[Fact]
+	public void SwitchesMultipleValuesReversedOrderImplicit () {
+		var parser = new ArgParser ( "-a 3 -a 2 -a 1 -a", null );
+		parser.RegisterSwitch ( 'a', "a", "0" );
+		parser.Present ( "-a" ).Should ().BeTrue ();
+		parser.HasValue ( "-a", true ).Should ().BeTrue ();
+		parser.Int ( "-a", "asdf" ).Should ().Be ( 1 );
+	}
+
+	[Fact]
+	public void ValueCanBeEnteredWithoutEquals () {
+		var parser = new ArgParser ( "-a 1 -b=2", null );
+		parser.RegisterSwitch ( 'a', "a", "0" );
+		parser.RegisterSwitch ( 'b', "b", "0" );
+		parser.Present ( "-a" ).Should ().BeTrue ();
+		parser.Present ( "-b" ).Should ().BeTrue ();
+		parser.Int ( "-a", "asdf" ).Should ().Be ( 1 );
+		parser.Int ( "-b", "bvcx" ).Should ().Be ( 2 );
+	}
+
+	[Fact]
+	public void DefaultValueIsKeptWithEqualSign () {
+		var parser = new ArgParser ( "-a= -b=5", null );
+		parser.RegisterSwitch ( 'a', "asdf", "2" );
+		parser.RegisterSwitch ( 'b', "bvcx", "3" );
+		parser.Int ( "-a", "aasdf" ).Should ().Be ( 2 );
+		parser.Int ( "-b", "bvcx" ).Should ().Be ( 5 );
+	}
+
+	[Fact]
+	public void SwitchAndArgumentCanHaveNegativeValue () {
+		var parser = new ArgParser ( "-a=-1 b -3 -c -5 d=-7", null );
+		parser.RegisterSwitch ( 'a', "a", "0" );
+		parser.RegisterSwitch ( 'b', "b", "0" );
+		parser.RegisterSwitch ( 'c', "cvbn", "0" );
+		parser.RegisterSwitch ( 'd', "d", "0" );
+		parser.Int ( "-a", "sdfa" ).Should ().Be ( -1 );
+		parser.Int ( "b", "b" ).Should ().Be ( -3 );
+		parser.Int ( "-c", "cvbn" ).Should ().Be ( -5 );
+		parser.Int ( "d", "d" ).Should ().Be ( -7 );
+	}
+
+
+
+	protected void AssertError ( Action action, string msg, int errCode ) =>
+		action.Should ().Throw<ArgumentException> ().Which.Message.Should ()
+		.StartWith ( msg ).And.Contain ( $"ArgParseErr#{errCode}" );
+}
