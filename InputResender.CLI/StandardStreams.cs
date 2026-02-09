50f92dc0d8268f0553ac20d4ba48dcaf52ddb263
xBecanto07 <ja.malinak@gmail.com>
2026-02-09 14:31:56 +0100

    Basic support for system testing + StatusInfo SCL test

---
+++ InputResender.CLI/StandardStreams.cs
@@ -0,0 +1,73 @@
+﻿using Components.Library;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace InputResender.CLI;
+public class StandardStream : IDisposable {
+	public IReadOnlyList<string> InBuffer => inBuffer.AsReadOnly();
+	public IReadOnlyList<string> OutBuffer => outBuffer.AsReadOnly();
+	public IReadOnlyList<string> ErrBuffer => errBuffer.AsReadOnly();
+
+	private List<string> inBuffer = new ();
+	private List<string> outBuffer = new ();
+	private List<string> errBuffer = new ();
+
+	readonly BlockingCollection<string> StdIn = new ();
+	readonly BlockingCollection<string> StdOut = new ();
+	readonly BlockingCollection<string> StdErr = new ();
+
+	public ConsoleManager ConsoleWrapper { get; init; }
+
+	public void InputLine ( string line ) {
+		lock ( inBuffer ) {
+			inBuffer.Add ( line );
+			StdIn.Add ( line );
+		}
+	}
+
+	public void Output ( string ss ) {
+		lock ( inBuffer ) {
+			if ( !StdOut.TryTake ( out string oldLine ) ) oldLine = string.Empty;
+			oldLine += ss;
+			outBuffer.Add ( ss );
+			StdOut.Add ( ss );
+		}
+	}
+
+	public void OutputLine ( string line ) {
+		lock ( outBuffer ) {
+			outBuffer.Add ( line );
+			StdOut.Add ( line );
+		}
+	}
+
+	public StandardStream () {
+		ConsoleWrapper = new (
+			realWriteLine: OutputLine
+			, realReadLine: StdIn.Take
+			, realWrite: Output
+			, realClear: null
+			, realReadChar: null
+			, realOverwriteLastLine: null
+			);
+	}
+
+	public string[] ReadAllOutput () {
+		lock ( outBuffer ) {
+			return outBuffer.ToArray ();
+		}
+	}
+
+	public void ClearOutput () {
+		lock ( outBuffer ) {
+			outBuffer.Clear ();
+		}
+	}
+
+	public void Dispose () {
+		StdIn.Dispose ();
+		StdOut.Dispose ();
+		StdErr.Dispose ();
+	}
+}
---
+++ InputResender.UnitTests/SystemTests/InputTransformation.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using FluentAssertions;
+using Components.Library;
+using System.Collections.Concurrent;
+using InputResender.CLI;
+using System.Collections.Generic;
+using InputResender.WindowsGUI.Commands;
+using System.Windows.Forms;
+using System;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+using Components.Implementations;
+using InputResender.UnitTests.IntegrationTests;
+
+namespace InputResender.UnitTests.SystemTests;
+public class InputTransformation : BaseSystemTest {
+	public InputTransformation ( ITestOutputHelper output ) : base ( output, "load sclModules", "load joiners" ) {
+	}
+
+	[Fact]
+	public void ShowInputStatusInfo () {
+		Test ( [
+			"seclav parse SIPtest.scl",
+			"SIP force",
+			"SIP assign SIPtest.scl",
+			"auto run setupPipes",
+			"hook add Pipeline Keydown KeyUp",
+			"sim keydown R",
+			], [
+				"ScriptedInputProcessor Status:"
+				, "InputCombination Count: 1" // It would be nice if multiple results could be merged into groups, where results inside of group would be order sensitive, individual groups wouldn't have to be. This would also allow to skip lines, group would be like Line1, Line 5. Order enforecd, whatever between is skipped. This could expand possibilities of the 'exclusive' sensitivity.
+			]
+			, TestSensitivity.None
+			, TestTimeout.Short );
+	}
+}
